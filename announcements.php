b63bfa31714be7657949ce76d00ce088ad905132
Kerelos <kero.diaa@gmail.com>
2017-05-21 15:20:28 +0200

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -15,7 +15,40 @@
     <tr>
     	<td height="90%">
         	<div id="Main_Body">
-        
+            <?php
+
+                $xml=("http://eng.cu.edu.eg/ar/category/credit-news/feed/");
+                $xmlDoc = new DOMDocument();
+                $xmlDoc->load($xml);
+
+                //get and output "<item>" elements
+                $x=$xmlDoc->getElementsByTagName('item');
+                echo("<table id='Timetable_Table'>");
+                for ($i=0; $i<=7; $i++) {
+                  $item_title=$x->item($i)->getElementsByTagName('title')
+                  ->item(0)->childNodes->item(0)->nodeValue;
+                  $item_link=$x->item($i)->getElementsByTagName('link')
+                  ->item(0)->childNodes->item(0)->nodeValue;
+                  $item_desc=$x->item($i)->getElementsByTagName('description')
+                  ->item(0)->childNodes->item(0)->nodeValue;
+                  $item_date=$x->item($i)->getElementsByTagName('pubDate')
+                  ->item(0)->childNodes->item(0)->nodeValue;
+
+                  $item_date=substr($item_date, 0, -5);
+                  echo("<tr>");
+                  echo ("<th><a style='text-decoration:blink;' href='" . $item_link
+                  . "'>" . $item_title . "</a></th>");
+                  echo('</tr>');
+                  echo('<tr>');
+                  echo("<td style='text-align:center;'>".$item_date."</td>");
+                  echo ("</tr>");
+                  echo("<tr>");
+                  echo ("<td style='text-align:right;'>".$item_desc."</td>");
+                  echo("</tr>");
+                }
+                echo('</table>');
+
+            ?>
         	</div>
         </td>
     </tr>
